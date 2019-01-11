1ae87ee23bffeeec4454f27d859bd4a30a6ac4f8
cmtp <christian.tola@hotmail.com>
2019-01-11 16:46:16 -0400

    Add Topics component

---
+++ .vuepress/components/Topics.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div>nodejs</div>
+        <div>Angular</div>
+        <div>React</div>
+        <div>Vue</div>
+        <div>HTML</div>
+        <div>CSS</div>
+        <div>Javascript</div>
+        <div>Databases</div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.container {
+    display: grid;
+}
+</style>
