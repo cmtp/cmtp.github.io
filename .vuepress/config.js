06b99b14da43f16fb60e3491bd04803f4fbc34f5
cmtp <christian.tola@hotmail.com>
2019-05-18 20:05:00 -0400

    development

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -10,7 +10,7 @@ module.exports = {
       }
     ],
     ["link", { rel: "manifest", href: "/manifest.json" }],
-    ['link', { rel: "icon", href: "favicon.ico" }],
+    ['link', { rel: "icon", href: "/favicon.ico" }],
     // ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
