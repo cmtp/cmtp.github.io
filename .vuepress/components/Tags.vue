387ab345c4045e6c6cd7726daa78f391b4923533
cmtp <christian@localhost.localdomain>
2020-04-19 17:32:24 -0400

    Modify Tag layout responsive

--- .vuepress/components/Tags.vue
+++ .vuepress/components/Tags.vue
@@ -62,6 +62,18 @@ export default {
   justify-content: space-between;
 }
 .blog-item {
+  width: 100%;
+}
+/* large desktop */
+@media screen and (min-width: 900px) {
+.blog-item {
   width: 30%;
+}
+}
+/* tablet */
+@media (max-width: 899px) and (min-width: 600px) {
+  .blog-item {
+    width: 45%;
+  }
 }
 </style>
